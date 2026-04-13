f64bde6c046c62bd2dea2a3ef6d329264a7e0f1b
Japneet Singh <138995439+Japneet1224@users.noreply.github.com>
2026-04-13 18:49:33 +0530

    Update config.js

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,3 @@
-// This checks if you are on Vite (import.meta.env) or Create React App (process.env)
+// src/config.js
 const API_URL = import.meta.env?.VITE_API_URL || process.env?.REACT_APP_API_URL || 'http://localhost:5000';
-
 export default API_URL;
